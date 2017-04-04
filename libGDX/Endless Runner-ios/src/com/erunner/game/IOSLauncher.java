5a95ddeee4c935c53e7a772a7e3a36bc41b427eb
up201306485 <up201306485@B03AV1205.salas.feupsig.fe.up.pt>
2017-04-04 16:09:45 +0100

    Minor improvements

--- libGDX/Endless Runner-ios/src/com/erunner/game/IOSLauncher.java
+++ libGDX/Endless Runner-ios/src/com/erunner/game/IOSLauncher.java
@@ -5,13 +5,13 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.erunner.game.Game;
+import com.erunner.game.PCMasterRace;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new Game(), config);
+        return new IOSApplication(new PCMasterRace(), config);
     }
 
     public static void main(String[] argv) {
