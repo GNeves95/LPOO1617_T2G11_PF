cd705b64e6c290dbb6f95e95a19097b6e05af4fb
GNeves95 <up201306485@fe.up.pt>
2017-04-04 17:04:57 +0100

    added code based on the classes example

--- libGDX/Endless Runner-core/src/com/erunner/game/PCMasterRace.java
+++ libGDX/Endless Runner-core/src/com/erunner/game/PCMasterRace.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class PCMasterRace extends ApplicationAdapter {
+public class PCMasterRace extends Game {
 	/**
 	 * Manages the game assets
 	 */
@@ -23,13 +23,15 @@ public class PCMasterRace extends ApplicationAdapter {
 
 	@Override
 	public void create () {
+		assetManager = new AssetManager();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
+		setScreen(new GameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0.9f, 0.9f, 1, 0.01f);
+		Gdx.gl.glClearColor(0.9f, 0.9f, 1, 0.1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
@@ -40,5 +42,13 @@ public class PCMasterRace extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+	
+	public AssetManager getAssetManager(){
+		return assetManager;
+	}
+	
+	public SpriteBatch getBatch(){
+		return batch;
 	}
 }
